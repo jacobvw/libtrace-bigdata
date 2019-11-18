d033785905390791e1cad07f11c8acd455ffcd77
Jacob van Walraven <jacobvw@icloud.com>
2019-11-18 02:03:07 +0000

    build debian package in a subdir

--- package-debian.sh
+++ package-debian.sh
@@ -4,8 +4,10 @@
 /bin/bash configure
 make
 make dist
-mv libtrace-bigdata-1.0.tar.gz libtrace-bigdata_1.0.orig.tar.gz
+mkdir debian-package
+mv libtrace-bigdata-1.0.tar.gz debian-package/libtrace-bigdata_1.0.orig.tar.gz
+cd debian-package
 tar -xf libtrace-bigdata_1.0.orig.tar.gz
-cp -r debian libtrace-bigdata-1.0/
+cp -r ../debian libtrace-bigdata-1.0/
 cd libtrace-bigdata-1.0
 debuild
