3fb441772f48bb46a4c111062675829591ee7e1e
Jacob van Walraven <jacobvw@icloud.com>
2019-12-18 09:44:08 +1300

    Add result set creation for http req/resp pairs, add tick event to cleanup old requests that do not receive a response

--- lib/module_http.cc
+++ lib/module_http.cc
@@ -5,6 +5,8 @@
 struct module_http_conf {
     bd_cb_set *callbacks;
     bool enabled;
+    int timeout_request;
+    int timeout_check;
 };
 static struct module_http_conf *config;
 
@@ -14,6 +16,7 @@ typedef struct module_http_header {
 } mod_http_header;
 
 typedef struct module_http_requests {
+    double start_ts;
     char *method;
     char *path;
     int version;
@@ -51,10 +54,16 @@ int module_http_packet(bd_bigdata_t *bigdata, void *mls) {
     mod_http_req *request;
     mod_http_stor *storage;
     uint64_t flow_id;
+    char buf2[100];
 
     storage = (mod_http_stor *)mls;
     payload = NULL;
     flow_id = bd_flow_get_id(bigdata->flow);
+
+    /* ensure a flow id was found */
+    if (flow_id == 0) {
+        return 0;
+    }
 
     layer3 = trace_get_layer3(bigdata->packet, &ethertype, &remaining);
     /* got layer3? */
@@ -113,6 +122,7 @@ int module_http_packet(bd_bigdata_t *bigdata, void *mls) {
                 fprintf(stderr, "Unable to allocate memory. func. module_http_packet()\n");
                 exit(BD_OUTOFMEMORY);
             }
+            request->start_ts = trace_get_seconds(bigdata->packet);
             request->method = strndup(method, method_len);
             request->path = strndup(path, path_len);
             request->version = minor_version;
@@ -145,34 +155,97 @@ int module_http_packet(bd_bigdata_t *bigdata, void *mls) {
             response_size = phr_parse_response(buf, remaining, &minor_version, &status,
                 &msg, &msg_len, headers, &num_headers, prevlen);
 
+            /* ensure the response was parsed correctly */
+            if (response_size) {
 
-            /* create a result set */
-            //bd_result_set_t *result_set = bd_result_set_create(bigdata, "http");
+                /* create a result set */
+                bd_result_set_t *result_set = bd_result_set_create(bigdata, "http");
 
-            //bd_result_set_insert_tag(result_set, "http_method", request->method);
-            //bd_result_set_insert_string(result_set, "http_path", request->path);
-            //bd_result_set_insert_int(result_set, "http_response_code", status);
+                /* insert request data */
+                bd_result_set_insert_tag(result_set, "request_method", request->method);
+                bd_result_set_insert_string(result_set, "request_path", request->path);
+                for (int i = 0; i != request->num_headers; ++i) {
+                    snprintf(buf2, sizeof(buf2), "%s_%s", "request", request->headers[i].name);
+                    bd_result_set_insert_string(result_set, buf2, request->headers[i].value);
+                    free(request->headers[i].name);
+                    free(request->headers[i].value);
+                }
 
-            fprintf(stderr, "got %s request to %s\n", request->method, request->path);
-            for (int i = 0; i != request->num_headers; ++i) {
-                printf("%s: %s\n", request->headers[i].name, request->headers[i].value);
-                free(request->headers[i].name);
-                free(request->headers[i].value);
+                /* insert response data */
+                bd_result_set_insert_int(result_set, "response_code", status);
+
+                /* insert the timestamp */
+                struct timeval tv = trace_get_timeval(bigdata->packet);
+                bd_result_set_insert_timestamp(result_set, tv.tv_sec);
+
+                /* publish the result set */
+                bd_result_set_publish(bigdata, result_set, tv.tv_sec);
+
+                /* remove from request from map */
+                storage->requests->erase(flow_id);
+                free(request->method);
+                free(request->path);
+                free(request);
             }
+        }
+    }
+}
 
+/* cleans out stored http requests that have not seen a response for the
+ * configured timeout period */
+int module_http_tick(bd_bigdata_t *bigdata, void *mls, uint64_t tick) {
 
+    mod_http_stor *storage = (mod_http_stor *)mls;
 
-            /* remove from request from map */
-            storage->requests->erase(flow_id);
-            free(request->method);
-            free(request->path);
-            free(request);
+    std::unordered_map<uint64_t, mod_http_req *>::iterator itr =
+        storage->requests->begin();
+    for (itr; itr != storage->requests->end(); ) {
+
+        mod_http_req *cur = (mod_http_req *)itr->second;
+
+        if (tick >= (cur->start_ts + config->timeout_request)) {
+
+            storage->requests->erase(itr++);
+            free(cur->method);
+            free(cur->path);
+            for (int i = 0; i != cur->num_headers; ++i) {
+                free(cur->headers[i].name);
+                free(cur->headers[i].value);
+            }
+        } else {
+            ++itr;
         }
     }
 }
 
 int module_http_stopping(void *tls, void *mls) {
 
+    mod_http_stor *storage = (mod_http_stor *)mls;
+
+    std::unordered_map<uint64_t, mod_http_req *>::iterator itr =
+        storage->requests->begin();
+    for (itr; itr != storage->requests->end(); itr++) {
+
+        mod_http_req *cur = (mod_http_req *)itr->second;
+
+        /* free request method/path */
+        free(cur->method);
+        free(cur->path);
+
+        /* free stored headers */
+        for (int i = 0; i != cur->num_headers; ++i) {
+            free(cur->headers[i].name);
+            free(cur->headers[i].value);
+        }
+    }
+
+    /* delete the map */
+    delete(storage->requests);
+
+    /* free the storage structure */
+    free(storage);
+
+    return 0;
 }
 
 int module_http_config(yaml_parser_t *parser, yaml_event_t *event, int *level) {
@@ -195,6 +268,26 @@ int module_http_config(yaml_parser_t *parser, yaml_event_t *event, int *level) {
                     }
                     break;
                 }
+                if (strcmp((char *)event->data.scalar.value, "timeout_request") == 0) {
+                    consume_event(parser, event, level);
+                    config->timeout_request = atoi((char *)event->data.scalar.value);
+                    if (config->timeout_request == 0) {
+                        fprintf(stderr, "Invalid timeout_request value. "
+                            "module_http. setting to default 20 seconds\n");
+                        config->timeout_request = 20;
+                    }
+                    break;
+                }
+                if (strcmp((char *)event->data.scalar.value, "timeout_check") == 0) {
+                    consume_event(parser, event, level);
+                    config->timeout_check = atoi((char *)event->data.scalar.value);
+                    if (config->timeout_check == 0) {
+                        fprintf(stderr, "Invalid timeout_check value. "
+                            "module_http. setting to default 20 seconds\n");
+                        config->timeout_check = 20;
+                    }
+                    break;
+                }
                 consume_event(parser, event, level);
                 break;
             default:
@@ -208,6 +301,10 @@ int module_http_config(yaml_parser_t *parser, yaml_event_t *event, int *level) {
         bd_register_packet_event(config->callbacks, (cb_packet)module_http_packet);
         bd_add_filter_to_cb_set(config->callbacks, "port 80");
         bd_register_stop_event(config->callbacks, (cb_stop)module_http_stopping);
+
+        /* set timeout check interval */
+        bd_register_tick_event(config->callbacks, (cb_tick)module_http_tick);
+        bd_add_tickrate_to_cb_set(config->callbacks, config->timeout_check);
 
         fprintf(stderr, "HTTP plugin enabled\n");
     }
@@ -225,6 +322,8 @@ int module_http_init(bd_bigdata_t *bigdata) {
 
     config->callbacks = bd_create_cb_set("http");
     config->enabled = 0;
+    config->timeout_request = 20;
+    config->timeout_check = 20;
 
     /* register for the config event */
     bd_register_config_event(config->callbacks, (cb_config)module_http_config);
